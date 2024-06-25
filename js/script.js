9b04b75e585c1ab85ec8f279883c378e8daa7da8
pollutedStock <pollutedstockpile@gmail.com>
2024-06-25 18:46:35 +0100

    Adding turn off button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-function play_sound(sound_id) {
-    var base_path = "assets/sounds/"
-    var sound = new Audio(base_path + sound_id + ".mp3");
+function play_sound(id) {
+    var base_path = "assets/"
+    var sound_dir = "sounds/"
+    var image_dir = "images/"
+
+    var sound = new Audio(base_path + sound_dir + id + ".mp3");
     sound.play();
+
+    const image = document.getElementById("screen-image");
+    image.src = base_path + image_dir + id + ".jpg";
+    image.style.display = "block";
+}
+
+function turn_off() {
+    const image = document.getElementById("screen-image");
+    image.style.display = "none";
 }
